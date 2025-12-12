21ed7162861156372d0f118fe93daf708d2feadd
Karel Psota <karel.psota@clevermaps.io>
2025-12-12 15:10:01 +0100

    duckdb init spatial fix

--- src/cm_data_transformation/db.py
+++ src/cm_data_transformation/db.py
@@ -1,8 +1,9 @@
 from sqlalchemy import text
 
-def init_duckdb(engine, sql_dir):
+def init_duckdb(engine):
     
     with engine.connect() as conn:
+        conn.execute(text("INSTALL spatial;"))
         conn.execute(text("LOAD spatial;"))
         conn.execute(text("INSTALL h3 FROM community;"))
         conn.execute(text("LOAD h3;"))
